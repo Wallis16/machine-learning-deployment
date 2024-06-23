118cc3ae89e8c495fc05548089fcf0e609ce2d8b
Wallis16 <diogeneswallis@outlook.com>
2024-06-22 22:41:08 -0400

    mlflow endpoint test

--- test/endpoints.py
+++ test/endpoints.py
@@ -1,2 +1,34 @@
-def test_always_passes():
+#mlflow
+
+import requests
+import json
+def test_training():
+    dataset = 'tlc_trip_data_1'
+    requests.get(f'http://localhost:8000/run_training/?collection_name={dataset}')
+    assert True
+
+def test_get_id():
+    server = "http://localhost:5000/"
+    experiment_name = "tlc_trip_data_1"
+    metric = "mean_score"
+    sort_by = "DESC"
+    requests.get(f'http://localhost:8000/get_run_id/?mlflow_server={server}&experiment_name={experiment_name}&metric={metric}&sort_by={sort_by}')
+    assert True
+
+def test_inference():
+    server = "http://localhost:5000/"
+    experiment_name = "tlc_trip_data_1"
+    metric = "mean_score"
+    sort_by = "DESC"
+    
+    run_id = requests.get(f'http://localhost:8000/get_run_id/?mlflow_server={server}&experiment_name={experiment_name}&metric={metric}&sort_by={sort_by}')
+    
+    requests.post(url = 'http://localhost:8000/inference/', data=json.dumps({
+    "logged_model": f"mlflow/data/mlartifacts/1/{run_id}/artifacts/model",
+    "inference_data": {
+    "passenger_count": [2],
+    "trip_distance": [23],
+    "payment_type": [1]
+    }
+    }))
     assert True
